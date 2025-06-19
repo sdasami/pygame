9a3fe6ead24a7ee23bd4c0d7e326274343ea6a7a
sda_sami <sami@salondigiapu.fi>
2025-06-19 10:40:23 +0300

    white player

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
+
 
 def main():
     pygame.init()
@@ -16,15 +14,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
